4e6b8e557c997faada249348edfd6ab6c44bf50d
Tobias Eiberle <utoppo@gmail.com>
2020-10-13 13:57:20 +0200

    make it work locally

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app
   .route('/')
   //GET REQUEST DRAW THE HOME PAGE
   .get(function(req, res) {
-    res.send('Please visit <a href="https://wppb.me">https://wppb.me</a>');
+    res.render('index');
     //res.redirect('https://wppb.me');
   }) // END GET ROUTE
 
